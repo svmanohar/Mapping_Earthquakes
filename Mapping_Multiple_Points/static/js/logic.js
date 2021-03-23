282e8df524f73983885722a6b089230c24dfd2e3
sasha manohar <vsman.dev@gmail.com>
2021-03-23 08:17:02 -0400

    Added multiple points to map with formatting

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -19,12 +19,36 @@ let map = L.map("mapid", {
   // Mapbox docs for tileLayer: https://leafletjs.com/reference-1.6.0.html#map-example
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
-L.circleMarker([34.0522, -118.2437], {
-  radius: 300,
-  color: "#000000",
-  fillColor: "#ffff00",
-  fillOpacity: 0.2
-}).addTo(map);
+// An array containing each city's location, state, and population.
+
+// re-assign the cities.js array into our script
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+
+  // access the location key in each city object and add to map with Leafly's function addTop
+  // use a circleMarker which we will vary the size based off population
+  L.circleMarker(city.location, {
+    radius: city.population/200000, // ensure the radii of each circle is divided by 100000 to maintain scale but make it readable on map, otherwise would be whole map size
+    color: "#FFA500",
+    fillColor: "#FFA500",
+    fillOpacity: 0.2,
+    weight: 4
+  })
+
+  // to add a popup to each marker, use Leafly's bindPopup() method: https://leafletjs.com/examples/quick-start/
+  // bindPopup() takes HTML in quotations for formatting as well as objects passed in
+  // starting a line with .<method or function> is the same as chaining it off the previous line's object, but enhances readability
+
+  // toLocaleString() method formats the population to be comma-separated
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+
+  // once we're done modifying the marker, we call the .addTo method and add it to our map object
+  .addTo(map);
+
+ });
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
