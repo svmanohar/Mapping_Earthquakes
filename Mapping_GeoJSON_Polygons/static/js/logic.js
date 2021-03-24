788dceb5d48a8db5bacc17459ea46d072d8d44f9
sasha manohar <vsman.dev@gmail.com>
2021-03-24 08:38:07 -0400

    Added neighborhood boundaries and tooltips for Toronto

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -44,7 +44,7 @@ let sanFranAirport =
 // let airportData = "https://raw.githubusercontent.com/svmanohar/Mapping_Earthquakes/main/majorAirports.json";
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/svmanohar/Mapping_Earthquakes/main/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/svmanohar/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Instantiate a geoJSON object through Leafly
 // Syntax: L.geoJSON(geojsonFeature).addTo(map);
@@ -141,7 +141,7 @@ let torontoData = "https://raw.githubusercontent.com/svmanohar/Mapping_Earthquak
 
 //  });
 
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     // set the maximum zoom parameters
     maxZoom: 18,
@@ -150,7 +150,7 @@ let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles
     });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -158,15 +158,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // create a base layer that contains both maps
 let baseMaps = {
-  Street: light,
-  Dark: dark
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
 };
 
-// Create the map object with center, zoom level and default layer. Focus on Toronto
+// Create the map object with center, zoom level and default layer. Focus on Toronto neighborhoods.
 let map = L.map('mapid', {
-  center: [44, -80],
-  zoom: 2,
-  layers: [light]
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map. Allows us to switch between layers
@@ -174,21 +174,19 @@ L.control.layers(baseMaps).addTo(map);
 
 // Create a style for the lines. geoJson can take an object containing styling key-value pairs instead of directly passing in styling 
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  fillColor: "#ffff00",
+  fillOpacity: 0.2,
+  weight: 1
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-// Apply styling and use onEachFeature() to access each line layer and bind a popup with information on each flight
-L.geoJson(data, {
-  style: myStyle,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-    + feature.properties.dst + "</h3>");
-  }
-})
-.addTo(map);
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3>" + feature.properties.AREA_NAME + "</h3>");
+    }
+  }).addTo(map)
 });
